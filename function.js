dafd4c196300341067b54751f276d16c5e89aee1
Cody Tacderas <codytacderas@Codys-MacBook-Air.local>
2017-06-19 11:49:26 -0600

    Query help

--- function.js
+++ function.js
@@ -3,7 +3,7 @@ $("#submit-button").on("click", function() {
 	event.preventDefault();
 	$("#gif-section").empty();
 	searchTerm = $("#search-input").val().trim();
-	var queryURL = "http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + searchTerm;
+	var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + searchTerm;
 	$.ajax({
 	  url: queryURL,
 	  method: "GET"
