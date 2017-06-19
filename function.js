0d1c8a27c950b07f227e5ea7b0c05d199ab490ec
Cody Tacderas <codytacderas@Codys-MacBook-Air.local>
2017-06-19 13:17:00 -0600

    With increased functionality

--- function.js
+++ function.js
@@ -1,29 +1,55 @@
-
 $("#submit-button").on("click", function() {
-	event.preventDefault();
-	$("#gif-section").empty();
-	searchTerm = $("#search-input").val().trim();
-	var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" + searchTerm;
-	$.ajax({
-	  url: queryURL,
-	  method: "GET"
-	})
-	.done(function(response) {
-	  var results = response.data;
-	  console.log(results);
-	  for (var i = 0; i < results.length; i++) {
-	    var gifDiv = $('<div class="item">');
-	    var rating = results[i].rating;
-	    console.log(results[i].rating);
-	    var p = $("<p>").text("Rating: " + rating);
-	    var imageUrl = response.data.image_original_url;
-	    console.log(imageUrl);
-	    var queryImage = $("<img>");
-        queryImage.attr("src", imageUrl);
-        queryImage.attr("alt", "gEtTin GiPhy");
-	    gifDiv.prepend(p);
-	    gifDiv.prepend(queryImage);
-	    $("#gif-section").prepend(gifDiv);
-	  }
-	});
+    event.preventDefault();
+    $("#gif-section").empty();
+    searchTerm = $("#search-input").val().trim();
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC&limit=10";
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function(response) {
+		var results = response.data;
+		for (var i = 0; i < results.length; i++) {
+			var gifDiv = $('<div class="item">');
+			var rating = results[i].rating;
+			var p = $("<p>").text("Rating: " + rating);
+			var imageUrl = results[i].images.fixed_height.url;
+			var queryImage = $("<img>");
+			queryImage.attr("src", imageUrl);
+			queryImage.attr("alt", "gEtTin GiPhy");
+			gifDiv.append(p);
+			gifDiv.append(queryImage);
+			$("#gif-section").append(gifDiv);
+			function createButton() {
+			    var newButton = document.createElement("button");
+			    $("#button-section").append(newButton);
+			    newButton.attr("src", results[i].images.fixed_height.url);
+			    newButton.attr("id", searchTerm);
+			}
+		}
+    });
+});
+
+$("button").on("click", function() {
+  $("#gif-section").empty();
+  var person = $(this).attr("data-person");
+  var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
+    person + "&api_key=dc6zaTOxFJmzC&limit=10";
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+    .done(function(response) {
+      var results = response.data;
+      for (var i = 0; i < results.length; i++) {
+        var gifDiv = $('<div class="item">');
+        var rating = results[i].rating;
+        var p = $("<p>").text("Rating: " + rating);
+        var personImage = $("<img>");
+        personImage.attr("src", results[i].images.fixed_height.url);
+        gifDiv.append(p);
+        gifDiv.append(personImage);
+        $("#gif-section").append(gifDiv);
+      }
+    });
 });
